4127cc1d8deafd59fc28aab7a610401733b87974
Thorben <ThoHa01@web.de>
2023-12-07 10:43:06 +0100

    fixed double code

--- API/src/app/app-routing.module.ts
+++ API/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import{EmployeeListComponent} from "./employee-list/employee-list.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/employee', pathMatch: 'full' },
-  { path: 'employee', component: EmployeeListComponent},
+  { path: '', redirectTo: '/employee', pathMatch: 'full' }
 ];
 
 @NgModule({
