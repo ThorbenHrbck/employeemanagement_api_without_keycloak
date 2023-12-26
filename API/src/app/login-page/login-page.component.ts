b1ecab624c82052972fd9879711c2170c7b28ac2
AntonFlpk <a.anton-ent@web.de>
2023-12-26 20:52:02 +0100

    added inputs and button to login page

--- API/src/app/login-page/login-page.component.ts
+++ API/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  constructor(private loginService: LoginService)
+  {}
 
+  username : string = '';
+  password : string = '';
+
+  inputUsername(username : string) : void 
+  {
+    this.username = username
+    console.log(this.username);
+  }
+
+  inputPassword(password : string) : void 
+  {
+    this.password = password
+    console.log(this.password)
+  }
+
+  btnLogin() : void 
+  {
+    
+  }
 }
